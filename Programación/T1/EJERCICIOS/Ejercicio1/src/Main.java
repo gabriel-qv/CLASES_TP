0318d3267b7a3497ad451bdc3974398b013b9187
Gabriel <gab18esp@gmail.com>
2025-10-10 00:41:11 +0200

    commit
    update

---
+++ Programación/T1/EJERCICIOS/Ejercicio1/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        String name;
+        int edad;
+        String city;
+
+        Scanner scn = new Scanner(System.in);
+        Scanner cit = new Scanner(System.in);
+
+        System.out.println("¿Cómo te llamas?");
+        name = scn.nextLine();
+
+        System.out.println("¿Cuántos años tienes?");
+        edad = scn.nextInt();
+
+        System.out.println("¿En qué ciudad vives?");
+        city = cit.nextLine();
+
+        System.out.println("Hola " + name + ", tienes " + edad + " y vives en la ciudad de " + city + ".");
+
+    }
+}
---
+++ Programación/T1/EJERCICIOS/Ejercicio2/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        int puntos;
+        puntos = 0;
+        int puntosuno, puntosdos, puntostres, puntofinal;
+
+        Scanner scnuno = new Scanner(System.in);
+        Scanner scndos = new Scanner(System.in);
+        Scanner scntres = new Scanner(System.in);
+
+        System.out.println("1ra modificación");
+        puntosuno = scnuno.nextInt();
+
+        System.out.println("2da moficicación");
+        puntosdos = scndos.nextInt();
+
+        System.out.println("3ra moficicación");
+        puntostres = scntres.nextInt();
+
+        System.out.println("Puntuación inicial: " + puntos);
+
+        puntofinal = puntos + puntosuno;
+        System.out.println("Después de primera modificación: " + puntofinal);
+
+        puntofinal = puntofinal + puntosdos;
+        System.out.println("Después de segunda modificación: " + puntofinal);
+
+        puntofinal = puntofinal + puntostres;
+        System.out.println("Puntuación final: " + puntofinal);
+
+    }
+}
