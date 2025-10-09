145a83f3e14cd89daad35166c81536e83a9d88db
Gabriel <gab18esp@gmail.com>
2025-10-09 20:18:40 +0200

    commmit
    asd

---
+++ Programación/T1/ProyectoUno/src/MainMetodos.java
@@ -0,0 +1,14 @@
+public class MainMetodos {
+    public static void main(String[] args){
+        System.out.println("Este es un metodo");
+        saludo("Cristhian");
+        despedida();
+    }
+    public static void saludo(String data){
+        System.out.println("Hola " + data + " este es tu primer metodo");
+    }
+
+    public static void despedida(){
+        System.out.println("Adios, hablamos pronto");
+    }
+}
